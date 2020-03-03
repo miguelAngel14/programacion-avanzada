facd511357393334f654b9492d34797a8f3b31d4
miguelAngel14 <61488303+miguelAngel14@users.noreply.github.com>
2020-03-02 22:14:33 -0600

    Add files via upload

---
+++ format(triangulo).py
@@ -0,0 +1,10 @@
+def main():
+    #los parámetros del format es de base de 1
+# utilizamos un flotante con decimales de 0,1
+  Baseint= 7
+  Alturaint = 5
+  calculodeareatriangulo=(intBase*intAltura)/2
+  txt = "Area:{4:0,1f} ({1} por {4} entre dos)"
+  print(txt.format(Baseint, Alturaint, calculodeAreaTriangulo))
+
+
